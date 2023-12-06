50d1d3526fe022c53ddb23ecb28887f5943f0978
CristopherLasluisa:) <132636689+Cristopher-10@users.noreply.github.com>
2023-12-05 22:02:54 -0500

    Delete listas.py

--- listas.py
+++
@@ -1,6 +0,0 @@
-#oredenamiento de números ingresados por el usuario con un rango de 6 datos
-awarded = []
-for i in range(6):
-    awarded.append(int(input("Introduce un número ganador: ")))
-awarded.sort()
-print("Los números ganadores son " + str(awarded))
