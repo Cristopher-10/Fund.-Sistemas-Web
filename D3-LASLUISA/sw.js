7228b7689e1c8299ebdfd98a35d505b560ec8931
CristopherLasluisa:) <132636689+Cristopher-10@users.noreply.github.com>
2023-12-11 22:32:52 -0500

    Add files via upload

---
+++ D3-LASLUISA/sw.js
@@ -0,0 +1,22 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/styles.css',
+          '/img/logo.png',
+          // Agrega más archivos que quieres cachear
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
